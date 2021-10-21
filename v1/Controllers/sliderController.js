67bc3367153479a4a448d54f10a398dbf45ff61f
amofficial888 <amofficial888@gmail.com>
2021-10-21 13:45:02 +0500

    Update sliderController.js

--- v1/Controllers/sliderController.js
+++ v1/Controllers/sliderController.js
@@ -20,7 +20,19 @@ function sliderheroactive() {
                 }
                 $.each(response, function (index, element) {
 
-                  $('#sliderappend').append('<div class="ec-slide-item swiper-slide d-flex" id=""><img src="https://controllers.eralive.net/images/E-Shop Banner Placeholder1.png"></div><div class="ec-slide-item swiper-slide d-flex"><img src="https://controllers.eralive.net/images/E-Shop Banner Placeholder2.png"></div>')
+                    if (index == 0) {
+                        
+                        imgas += '<div class="ec-slide-item swiper-slide d-flex swiper-slide-active">' +
+                            '<img src="' + element.src + '">' +
+                            '</div>';
+                        
+                    }
+                    else {
+                        
+                        imgas += '<div class="ec-slide-item swiper-slide d-flex">' +
+                        '<img src="' + element.src + '">' +
+                        '</div>';
+                    }
 
                 });
                 $('#sliders_items_imgs').append(imgas);
